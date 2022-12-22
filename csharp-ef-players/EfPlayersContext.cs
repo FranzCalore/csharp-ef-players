15827d7f35117a39b348820fe5ec3462fe4ce542
FranzCalore <franzkaiser1993@gmail.com>
2022-12-22 14:38:49 +0100

    Completata la milestone0

---
+++ csharp-ef-players/EfPlayersContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_ef_players
+{
+    public class EfPlayersContext:DbContext
+    {
+        DbSet<FootballPlayer> footballPlayers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = localhost; Initial Catalog = dbPlayers; Integrated Security = True; Pooling = False;TrustServerCertificate=True");
+        }
+    }
+}
---
+++ csharp-ef-players/FootballPlayer.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_ef_players
+{
+    [Table("FootballPlayer")]
+    [Index(nameof(player_Id))]
+    public class FootballPlayer
+    {
+        [Key]
+        public int player_Id { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        public string surname { get; set; }
+
+        [Required]
+        public int vote { get; set; }
+
+        [Required]
+        public int gameNumber { get; set; }
+
+        [Required]
+        public int winNumber { get; set; }
+
+    }
+}
